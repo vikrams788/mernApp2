e78949e6bad0b92cb6706ed35244b635588b4198
unknown <vikrams788058@gmail.com>
2024-03-21 14:49:15 +0530

    vercel config

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://mern-app2-enes.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }));
--- backend/utils/tokenUtils.js
+++ backend/utils/tokenUtils.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.generateTokens = (userId) => {
     const accessToken = jwt.sign({ userId }, process.env.JWT_SECRETKEY, { expiresIn: '5d' });
-    const refreshToken = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRETKEY, { expiresIn: '14d' }); // Use a different secret key for refresh tokens
+    const refreshToken = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRETKEY, { expiresIn: '14d' });
 
     return { accessToken, refreshToken };
 };
